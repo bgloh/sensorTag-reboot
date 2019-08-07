e8c7a1b924d019c2fbbca01287e439552ec9a8d4
bgloh <byoungloh@gmail.com>
2019-08-08 08:17:57 +0900

    Create pwmTask.h
    pwm task header file

---
+++ cc2650_Launchpad/pwmTask.h
@@ -0,0 +1,10 @@
+//pwmTask.h
+
+#ifndef PWMTASK_H_
+#define PWMTASK_H_
+
+
+/* Initializes the PwmLed Task and creates all TI-RTOS objects */
+extern void PwmTask_create(void);
+
+#endif /* PWMTASK_H_ */
